ffe6fd3af8c9897c64dcc3f76f6c5993ee4778c9
Lucas Cunha <lucas.cunha@inpartec.com.br>
2019-05-12 20:19:56 -0300

    Primeiro commit, estrutura basica

---
+++ src/mothersday/Main.java
@@ -0,0 +1,13 @@
+package mothersday;
+
+import static java.lang.System.out;
+import mothersday.users.*;
+public class Main
+{
+   public static void main(final String[] arguments)
+   {
+        User user = new User();
+        System.out.println("Hello main");
+        System.out.println(user.toString());
+   }
+}
---
+++ src/mothersday/users/User.java
@@ -0,0 +1,9 @@
+package mothersday.users;
+public class User
+{
+   @Override
+   public String toString()
+   {
+      return "I'm the Parent of users.";
+   }
+}
