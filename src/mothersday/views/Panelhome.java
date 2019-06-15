29d1f0f5799c0b8708b18aa93857b3df9da8a688
lucas <lucascercun@gmail.com>
2019-06-15 20:15:27 -0300

    display son

--- src/mothersday/views/Panelhome.java
+++ src/mothersday/views/Panelhome.java
@@ -42,20 +42,17 @@ public class Panelhome extends javax.swing.JFrame {
         jTabbedPaneContainer.add("Admin", jPanelAdmin);
             
       
-        jLabel3 = new javax.swing.JLabel();
-        jTextFieldCargo = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jTextFieldTipoSanguineo = new javax.swing.JTextField();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextAreaObservacao = new javax.swing.JTextArea();
-        jLabel1 = new javax.swing.JLabel();
-        jTextFieldNome = new javax.swing.JTextField();
+        jLabel3 = new JLabel();
+        jTextFieldCargo = new JTextField();
+        jLabel4 = new JLabel();
+        jTextFieldTipoSanguineo = new JTextField();
+        jScrollPane1 = new JScrollPane();
+        jTextAreaObservacao = new JTextArea();
+        jLabel1 = new JLabel();
+        jTextFieldNome = new JTextField();
          
-    
         jLabel1.setText("Nome");
         jLabel4.setText("Tipo Sangu\u00edneo");
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Home");
         jLabel3.setText("Cargo");
         jTextAreaObservacao.setColumns(20);
         jTextAreaObservacao.setRows(5);
@@ -85,11 +82,11 @@ public class Panelhome extends javax.swing.JFrame {
             .addGroup(jPanelFilhoLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle
+                .addPreferredGap(LayoutStyle
                 .ComponentPlacement.RELATED)
                 .addComponent(jTextFieldNome, javax.swing
-                .GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout
-                .DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .GroupLayout.PREFERRED_SIZE, GroupLayout
+                .DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(28, Short.MAX_VALUE))
         );
          
@@ -111,20 +108,20 @@ public class Panelhome extends javax.swing.JFrame {
         );
         jPanelMotherLayout.setVerticalGroup(
             jPanelMotherLayout.createParallelGroup
-            (javax.swing.GroupLayout.Alignment.LEADING)
+            (GroupLayout.Alignment.LEADING)
             .addGroup(jPanelMotherLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.
+                .addPreferredGap(LayoutStyle.
                 ComponentPlacement.RELATED)
-                .addComponent(jTextFieldCargo, javax.swing.
-                GroupLayout.PREFERRED_SIZE, javax.swing.
-                GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.
+                .addComponent(jTextFieldCargo, 
+                GroupLayout.PREFERRED_SIZE, 
+                GroupLayout.DEFAULT_SIZE, GroupLayout.
                 PREFERRED_SIZE)
                 .addContainerGap(28, Short.MAX_VALUE))
         );
          
-        javax.swing.GroupLayout jPanelPessoalLayout = new javax.
+        GroupLayout jPanelPessoalLayout = new javax.
         swing.GroupLayout(jPanelAdmin);
         jPanelAdmin.setLayout(jPanelPessoalLayout);
         jPanelPessoalLayout.setHorizontalGroup(
@@ -133,17 +130,17 @@ public class Panelhome extends javax.swing.JFrame {
             .addGroup(jPanelPessoalLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanelPessoalLayout.createParallelGroup
-                (javax.swing.GroupLayout.Alignment.TRAILING, false)
+                (GroupLayout.Alignment.TRAILING, false)
                     .addComponent(jTextFieldTipoSanguineo, javax
                     .swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4, javax.swing.GroupLayout
-                    .Alignment.LEADING, javax.swing.GroupLayout
-                    .DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, 
+                    .addComponent(jLabel4, GroupLayout
+                    .Alignment.LEADING, GroupLayout
+                    .DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, 
                     Short.MAX_VALUE))
                 .addContainerGap(255, Short.MAX_VALUE))
         ); 
         jPanelPessoalLayout.setVerticalGroup(
-            jPanelPessoalLayout.createParallelGroup(javax.swing.GroupLayout
+            jPanelPessoalLayout.createParallelGroup(GroupLayout
             .Alignment.LEADING)
             .addGroup(jPanelPessoalLayout.createSequentialGroup()
                 .addContainerGap()
